d63ec6b6573e62358d6ac4f2fe879aeeea29baa4
Vo Duy Tuan <tuanmaster2012@gmail.com>
2016-07-11 09:45:38 -0700

    first commit

---
+++ cronjob.php
@@ -0,0 +1,5 @@
+<?php
+
+$result = file_get_contents('http://requestb.in/t6amlit6');
+    echo $result;
+
---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Replace environment COLLECTD_WRITEHTTP_HOST
+cp /etc/collectd/configs/collectd-config.conf.tpl /etc/collectd/collectd.conf.tpl
+envtpl /etc/collectd/collectd.conf.tpl
+
+cron
+
+supervisord -n
+
+
